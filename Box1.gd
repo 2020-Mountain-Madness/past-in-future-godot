88b81413127e89fc5fc86f946dabbc8ad0c822b4
Unknown <hardyshongyijin@gmail.com>
2020-02-23 03:25:21 -0800

    Pickable Boxes

--- Box1.gd
+++ Box1.gd
@@ -1,15 +1,29 @@
 extends RigidBody2D
 
+export (bool) var from_future = false
 var picked = false
+
+onready var pick_btn = "future_pick" if from_future else "past_pick"
+onready var player = "FuturePlayer" if from_future else "Player"
+onready var player_node = get_node("../FuturePlayer") if from_future else get_node("../Player")
 
 func _physics_process(delta):
 	if picked == true:
-		self.position = get_node("../FuturePlayer/Position2D").global_position
+		self.position = player_node.global_position
+		self.position.y -= 18
+		sleeping = true
+	else:
+		sleeping = false
 
 
 func _input(event):
-	if Input.is_action_just_pressed("future_pick"):
+	if Input.is_action_pressed(pick_btn) and not picked:
 		var bodies = $Detector.get_overlapping_bodies()
 		for body in bodies:
-			if body.name == "FuturePlayer":
+			print(body.name)
+			if body.name == player:
 				picked = true
+	elif Input.is_action_pressed(pick_btn) and picked:
+		picked = false
+		self.position = player_node.global_position
+		self.position.x += player_node.facing * 18
